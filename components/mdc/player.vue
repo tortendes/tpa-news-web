730987ee1bedda164f9657d633250e7899ec5802
Torten Kiritsugu <me@torten.xyz>
2025-05-26 04:06:30 +0800

    [change] changed player heads from 2d to 3d

--- components/mdc/player.vue
+++ components/mdc/player.vue
@@ -20,10 +20,12 @@ const { data: player, status } = await useLazyFetch<Response>(`https://playerdb.
 </script>
 
 <template>
-    <div class="inline-flex items-center justify-center gap-0.5">
-        <NuxtImg v-if="status == 'success' && player" :src="`https://crafatar.com/renders/head/${player.data.player.id}?size=48&overlay=true`" class="w-auto h-4" />
+    <div class="inline-flex items-center justify-center gap-1">
+        <NuxtImg v-if="status == 'success' && player" :src="`https://crafatar.com/avatars/${player.data.player.id}?size=48&overlay=true`" class="w-auto h-4" />
         <NuxtImg v-else-if="status == 'pending'" src="https://images.minecraft-heads.com/render3d/head/32/3286f5a31d2d4fc6090236e6f26a84d9.webp" class="w-auto h-4" />
         <NuxtImg v-else src="https://images.minecraft-heads.com/render3d/head/32/325f5f7aedf3030e704e157eb1fa437f.webp" class="w-auto h-4" />
-        <slot />
+        <div class="">
+            <slot />
+        </div>
     </div>
 </template>

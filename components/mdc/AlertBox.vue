cc983b512666e9b43392dd1af8cc6e63e1b61bf3
Torten Kiritsugu <me@torten.xyz>
2025-06-24 05:27:32 +0800

    [feat] added infoboxes

--- components/mdc/AlertBox.vue
+++ components/mdc/AlertBox.vue
@@ -1,15 +1,30 @@
+<script setup lang="ts">
+const props = defineProps<{ 
+    title: string,
+    variant: 'info' | 'warn' | 'error' | 'default'
+}>()
+
+</script>
+
 <template>
-    <div class="px-4 py-2 flex gap-2 border-[1px] items-center border-yellow-500 mx-4">
-        <span class="text-yellow-500">
-            <UIcon name="line-md:alert-circle" class="size-8" />
-        </span>
-        <div class="flex flex-col">
-            <h1 class="text-xl">
-                <slot name="title" mdc-unwrap="p" />
-            </h1>
-            <p class="text-sm text-gray-600">
-                <slot name="description" mdc-unwrap="p" />
-            </p>
+    <div class="border-[1px]">
+        <div v-if="props.variant === 'info'" class="px-2 py-1 flex gap-1 items-center bg-blue-500 text-white">
+            <UIcon name="lucide:info" class="size-16" />
+            <span class="font-mono font-medium">{{ props.title }}</span>
+        </div>
+        <div v-else-if="props.variant === 'warn'" class="px-2 py-1 flex gap-1 items-center bg-yellow-400 text-white">
+            <UIcon name="lucide:triangle-alert" class="size-16" />
+            <span class="font-mono font-medium">{{ props.title }}</span>
+        </div>
+        <div v-else-if="props.variant === 'error'" class="px-2 py-1 flex gap-1 items-center bg-red-500 text-white">
+            <UIcon name="lucide:circle-alert" class="size-16" />
+            <span class="font-mono font-medium">{{ props.title }}</span>
+        </div>
+        <div v-else class="px-2 py-1 flex gap-1 items-center bg-gray-600 text-white">
+            <span class="font-mono font-medium">{{ props.title }}</span>
+        </div>
+        <div class="px-2 py-0.5">
+            <slot />
         </div>
     </div>
 </template>

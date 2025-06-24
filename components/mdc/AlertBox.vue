a0f94accf44b5f5954dc11f1b72d0f4575fd555f
Torten Kiritsugu <me@torten.xyz>
2025-06-25 05:05:30 +0800

    [fix] links on tips

--- components/mdc/AlertBox.vue
+++ components/mdc/AlertBox.vue
@@ -8,20 +8,20 @@ const props = defineProps<{
 
 <template>
     <div class="border-[1px]">
-        <div v-if="props.variant === 'info'" class="px-2 py-1 flex gap-1 items-center bg-blue-500 text-white">
+        <div v-if="props.variant === 'info'" class="px-2 py-1 flex gap-1 items-center bg-blue-700 text-white">
             <UIcon name="lucide:info" class="size-16" />
-            <span class="font-mono font-medium">{{ props.title }}</span>
+            <span class="font-medium">{{ props.title }}</span>
         </div>
         <div v-else-if="props.variant === 'warn'" class="px-2 py-1 flex gap-1 items-center bg-yellow-400 text-white">
             <UIcon name="lucide:triangle-alert" class="size-16" />
-            <span class="font-mono font-medium">{{ props.title }}</span>
+            <span class="font-medium">{{ props.title }}</span>
         </div>
         <div v-else-if="props.variant === 'error'" class="px-2 py-1 flex gap-1 items-center bg-red-500 text-white">
             <UIcon name="lucide:circle-alert" class="size-16" />
-            <span class="font-mono font-medium">{{ props.title }}</span>
+            <span class="font-medium">{{ props.title }}</span>
         </div>
         <div v-else class="px-2 py-1 flex gap-1 items-center bg-gray-600 text-white">
-            <span class="font-mono font-medium">{{ props.title }}</span>
+            <span class="font-medium">{{ props.title }}</span>
         </div>
         <div class="px-2 py-0.5">
             <slot />
